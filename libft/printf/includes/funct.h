05f4b1055a2c0569b86b8cbb897e8ae44f6d03c0
Moonflakes <mthiery@student.42.fr>
2018-08-03 16:53:06 +0200

    modif funct.h avec cond2

--- libft/printf/includes/funct.h
+++ libft/printf/includes/funct.h
@@ -5,15 +5,15 @@
 
 t_tf  tabf[] =
 {
-    {"xX\0", "xXeEgG\0", &print_hex},
-    {"cC\0", "xXeEgG\0", &print_char},
-    {"dDi\0", "xXeEgG\0", &print_nb},
-    {"sS\0", "xXeEgG\0", &print_string},
-    {"fF\0", "xXeEgG\0", &print_f},
-    {"aA\0", "xXeEgG\0", &if_length},
-    {"p\0", "xXeEgG\0", &print_p},
-  //  {"eE\0", "xXeEgG\0", &print_e},
-    //{"gG\0", "xXeEgG\0", &print_g},
+    {"cC\0", 0, &print_char},
+    {"dDi\0", 0, &print_nb},
+    {"sS\0", 0, &print_string},
+    {"fF\0", 0, &print_f},
+    {"aA\0", 0, &if_length},
+    {"p\0", 0, &print_p},
+    {"xX\0", 1, &print_hex},
+    {"eE\0", 1, &print_e},
+    {"gG\0", 1, &print_g},
     {NULL, NULL, NULL}
 };
 
